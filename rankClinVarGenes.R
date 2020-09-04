dc2ce83f9bd8e606309a4da73a24b28415dd8e92
Kevin Kuang <kvnkuang@users.noreply.github.com>
2020-09-04 11:36:49 -0400

    Update script to report both missense variants and VUS

--- rankClinVarGenes.R
+++ rankClinVarGenes.R
@@ -11,11 +11,11 @@ if (!hasFilteredList) {
   
   # Filter out variants that are
   # 1) missense variants, and
-  filteredVariants = variants[str_detect(Name, "\\(p\\..*\\)") & 
+  missenseVariants = variants[str_detect(Name, "\\(p\\..*\\)") & 
                                 !str_detect(Name, "\\(p\\..*(=|Ter)\\)")]
   
   # 2) are classified to have uncertain significance
-  filteredVariants = filteredVariants[str_detect(ClinicalSignificance, "Uncertain significance")]
+  filteredVariants = missenseVariants[str_detect(ClinicalSignificance, "Uncertain significance")]
   
   # Here is a list of clinical significance categories that were removed after the filter
   # Uncertain significance
@@ -23,27 +23,26 @@ if (!hasFilteredList) {
   # Uncertain significance, drug response
   # Uncertain significance, other
   
-  # Select submissions that are collected through clinical testing and not through curating the literature
-  ids = filteredVariants[, unique(VariationID)]
-  filteredSubmissions = submissions[`#VariationID` %in% ids & 
-                                      str_detect(CollectionMethod, "clinical testing") & 
-                                      !str_detect(CollectionMethod, "literature only"), 
-                                    .(var_id = `#VariationID`, gene_symbol = SubmittedGeneSymbol, 
-                                      submitter = Submitter, scv = SCV)]
+  # Select variants that are collected through clinical testing and not through curating the literature
+  selectClinicalVariants = function(sourceVariants) {
+    ids = sourceVariants[, unique(VariationID)]
+    filteredSubmissions = submissions[`#VariationID` %in% ids & 
+                                        str_detect(CollectionMethod, "clinical testing") & 
+                                        !str_detect(CollectionMethod, "literature only"), 
+                                      .(var_id = `#VariationID`, gene_symbol = SubmittedGeneSymbol, 
+                                        submitter = Submitter, scv = SCV)]
+    filteredSubmissions = filteredSubmissions[gene_symbol != "-"]
+    ids = unique(filteredSubmissions$var_id)
+    
+    return(sourceVariants[VariationID %in% ids])
+  }
+  missenseVariants = selectClinicalVariants(missenseVariants)
+  filteredVariants = selectClinicalVariants(filteredVariants)
   
   # Here is a list of collection methods that were removed after the filter
   # clinical testing
   # clinical testing;provider interpretation
   # clinical testing;curation
-  
-  # Remove "-" in gene name
-  filteredSubmissions = filteredSubmissions[gene_symbol != "-"]
-  
-  # Remove some columns to save space
-  filteredVariants$PhenotypeList = NULL
-  filteredVariants$PhenotypeIDS = NULL
-  filteredVariants$RCVaccession = NULL
-  filteredVariants$OtherIDs = NULL
   
   # Save the filtered list
   fwrite(filteredVariants, "filtered_variants.csv")
@@ -51,12 +50,26 @@ if (!hasFilteredList) {
   filteredVariants = fread("filtered_variants.csv")
 }
 
-# Rank genes by their number of unique missense VUS variants
-rankedGenes = unique(filteredVariants[HGNC_ID != "-" & !str_detect(GeneSymbol, "subset|cover"),
-                                      .(GeneSymbol, Name, HGNC_ID)])
-rankedGenes = rankedGenes[, .(num_variants = .N), by = c("GeneSymbol", "HGNC_ID")]
-rankedGenes = rankedGenes[order(num_variants, decreasing = T)]
-rankedGenes = rankedGenes[, .(symbol = GeneSymbol, hgnc_id = HGNC_ID, num_missense_vus_variants = num_variants)]
+# Count unique variants
+countUniqueVariants = function(sourceVariants) {
+  counts = unique(sourceVariants[HGNC_ID != "-" & !str_detect(GeneSymbol, "subset|cover"),
+                                 .(GeneSymbol, Name, HGNC_ID)])
+  counts = counts[, .(num_variants = .N), by = c("GeneSymbol", "HGNC_ID")]
+  
+  return(counts)
+}
+
+# Count unique missense and missense VUS variants
+missenseCount = countUniqueVariants(missenseVariants)
+vusCount= countUniqueVariants(filteredVariants)
+merged = merge(missenseCount, vusCount, by = c("GeneSymbol", "HGNC_ID"), all = T)
+for (j in names(merged)) set(merged,which(is.na(merged[[j]])),j,0)
+merged = merged[, .(symbol = GeneSymbol, hgnc_id = HGNC_ID, 
+                    num_unique_missense = num_variants.x,
+                    num_unique_missense_vus = num_variants.y)]
+
+# Rank by the number of unique missnese VUS variants
+rankedGenes = merged[order(num_unique_missense_vus, decreasing = T)]
 
 # Save the ranked
 fwrite(rankedGenes, "genes_ranked_filtered_variants.csv")
